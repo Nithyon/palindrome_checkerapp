d2c6879f03c84335a72e53320dd94f9137fd607e
GitHub User <user@github.com>
2026-03-10 01:44:20 +0530

    UC01: application entry and welcome message

--- app/src/UseCase1PalindromeCheckerApp.java
+++ app/src/UseCase1PalindromeCheckerApp.java
@@ -1,14 +1,32 @@
+/**
+ * MAIN CLASS - UseCase1PalindromeCheckerApp
+ *
+ * Use Case 1: Application Entry & Welcome Message
+ *
+ * Description:
+ * This class represents the entry point of the
+ * Palindrome Checker Management System.
+ *
+ * The application:
+ * - Starts execution from the main() method
+ * - Displays a welcome message
+ * - Shows application version
+ *
+ * No palindrome logic is implemented yet.
+ */
+
 public class UseCase1PalindromeCheckerApp {
+
+    /**
+     * Application entry point.
+     * This is the first method executed by the JVM.
+     */
 
     public static void main(String[] args) {
 
-        // Basic hardcoded input
-        String input = "madam";
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully.");
 
-        String reversed = new StringBuilder(input).reverse().toString();
-        boolean isPalindrome = input.equals(reversed);
-
-        System.out.println("Input: " + input);
-        System.out.println("Is Palindrome? : " + isPalindrome);
     }
 }
