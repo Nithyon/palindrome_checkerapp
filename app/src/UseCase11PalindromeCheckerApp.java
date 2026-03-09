2e4719337ece9af9348b6488a19c646008a5b2ea
GitHub User <user@github.com>
2026-03-10 01:20:27 +0530

    Implement palindrome apps for use cases 10 to 13

--- app/src/UseCase11PalindromeCheckerApp.java
+++ app/src/UseCase11PalindromeCheckerApp.java
@@ -1,4 +1,45 @@
 package PACKAGE_NAME;
 
+import java.util.Scanner;
+
 public class UseCase11PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        PalindromeService service = new PalindromeService();
+        boolean result = service.checkPalindrome(input);
+
+        if (result) {
+            System.out.println("Is Palindrome? : true");
+        } else {
+            System.out.println("Is Palindrome? : false");
+        }
+
+        scanner.close();
+    }
+}
+
+class PalindromeService {
+
+    public boolean checkPalindrome(String input) {
+
+        int start = 0;
+        int end = input.length() - 1;
+
+        while (start < end) {
+
+            if (input.charAt(start) != input.charAt(end)) {
+                return false;
+            }
+
+            start++;
+            end--;
+        }
+
+        return true;
+    }
 }
--- app/src/UseCase13PalindromeCheckerApp.java
+++ app/src/UseCase13PalindromeCheckerApp.java
@@ -1,4 +1,42 @@
 package PACKAGE_NAME;
 
+import java.util.Scanner;
+
 public class UseCase13PalindromeCheckerApp {
+
+    public static boolean checkPalindrome(String input) {
+
+        int start = 0;
+        int end = input.length() - 1;
+
+        while (start < end) {
+
+            if (input.charAt(start) != input.charAt(end)) {
+                return false;
+            }
+
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        long startTime = System.nanoTime();
+        boolean result = checkPalindrome(input);
+        long endTime = System.nanoTime();
+
+        long executionTime = endTime - startTime;
+
+        System.out.println("Is Palindrome? : " + result);
+        System.out.println("Execution Time : " + executionTime + " ns");
+
+        scanner.close();
+    }
 }
